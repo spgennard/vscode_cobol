86cd9829bba0da6dd2bb16974b89ae40df494d41
Stephgen Gennard <stephen@gennard.net>
2019-07-21 20:14:07 +0100

    add package version

--- publishit.sh
+++ publishit.sh
@@ -7,9 +7,11 @@ vsce publish
 rm -f *.vsix
 vsce package
 COMMIT_LOG=$(git log -1 --format='%ci %H %s')
+PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 github-release upload \
   --owner=spgennard \
   --repo=vscode_cobol \
   --tag="latest" \
+  --name=$PACKAGE_VERSION \
   --body="${COMMIT_LOG}" \
   cobol*.vsix
