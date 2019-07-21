7abe615dcf2fb349e8b365eb247446a5ddf52226
Stephgen Gennard <stephen@gennard.net>
2019-07-21 20:06:51 +0100

    update

--- publishit.sh
+++ publishit.sh
@@ -4,3 +4,12 @@ git clean -fdx
 npm install
 git push
 vsce publish
+rm -f *.vsix
+vsce package
+COMMIT_LOG=$(git log -1 --format='%ci %H %s')
+github-release upload \
+  --owner=spgennard \
+  --repo=vscode_cobol \
+  --tag="latest" \
+  --body="${COMMIT_LOG}" \
+  cobol*.vsix
