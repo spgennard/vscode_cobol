8bb8d3fa3e78616e30cadd9132383ec39e882a05
Stephgen Gennard <stephen@gennard.net>
2019-12-15 15:25:47 +0000

    dev: rename usage provider to be a linter provide  - duplicate iconfiguration.ts into src/server/src

---
+++ src/server/src/iconfiguration.ts
@@ -0,0 +1,77 @@
+export enum outlineFlag {
+    On = "on",
+    Off = "off",
+    Partial = "partial",
+    Skeleton = "skeleton"
+}
+
+
+export interface ICOBOLSettings {
+    experimential_features: boolean;
+    enable_tabstop: boolean;
+    pre_parse_line_limit: number;
+    ignorecolumn_b_onwards: boolean;
+    copybooks_nested:boolean;
+    fuzzy_variable_search: boolean;
+    cache_metadata: string;
+    outline: outlineFlag;
+    copybookdirs: string[];
+    copybookexts: string[];
+    tabstops: number[];
+    linter: boolean;
+}
+
+
+export class COBOLSettings implements ICOBOLSettings {
+    public tabstops: number[];
+    public copybookexts: string[];
+    public copybooks_nested: boolean;
+    public experimential_features: boolean;
+    public enable_tabstop: boolean;
+    public pre_parse_line_limit: number;
+    public ignorecolumn_b_onwards: boolean;
+    public fuzzy_variable_search: boolean;
+    public cache_metadata: string;
+    public outline: outlineFlag;
+    public copybookdirs: string[];
+    public linter: boolean;
+
+    constructor() {
+        this.experimential_features = false;
+        this.enable_tabstop = true;
+        this.pre_parse_line_limit = 25;
+        this.ignorecolumn_b_onwards = false;
+        this.copybooks_nested = false;
+        this.fuzzy_variable_search = false;
+        this.cache_metadata = "off";
+        this.outline = outlineFlag.Off;
+        this.copybookdirs = [];
+        this.copybookexts = [];
+        this.tabstops = [];
+        this.linter = false;
+    }
+}
+
+export class COBOLSettingsHelper {
+    public static isCachingEnabled(setting: ICOBOLSettings): boolean {
+        var cacheEnum = setting.cache_metadata;
+    
+        switch (cacheEnum) {
+            case "on": return true;
+            case "partial": return true;
+            case "off": return false;
+        }
+        return false;
+    }
+    
+    public static isCachingSetToON(setting: ICOBOLSettings): boolean {
+        var cacheEnum = setting.cache_metadata;
+        switch (cacheEnum) {
+            case "on": return true;
+            case "partial": return false;
+            case "off": return false;
+        }
+        return false;
+    }
+
+}
