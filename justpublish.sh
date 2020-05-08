ea65db01af916de2d25ca44c972d134d77c4bbe2
Stephgen Gennard <stephen@gennard.net>
2020-05-08 19:47:35 +0100

    push

---
+++ justpublish.sh
@@ -0,0 +1,19 @@
+# exit on error
+vsce publish
+rm -f *.vsix
+vsce package
+COMMIT_LOG=$(git log -1 --format='%ci %H %s')
+PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+github-release upload \
+  --owner=spgennard \
+  --repo=vscode_cobol \
+  --tag="$PACKAGE_VERSION" \
+  --name=$PACKAGE_VERSION \
+  --body="${COMMIT_LOG}" \
+  cobol*.vsix
+
+npx ovsx publish cobol*.vsix -p $(cat $HOME/.ovsx.token)
+
+npm-check-updates
+
+echo "use: ncu -u"
