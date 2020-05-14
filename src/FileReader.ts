3c52cc9204a057864916755d0e1995c5674dd575
Stephgen Gennard <stephen@gennard.net>
2020-05-14 15:18:26 +0100

    fix change in fs

--- src/FileReader.ts
+++ src/FileReader.ts
@@ -4,12 +4,17 @@ import path = require("path");
 import { getCurrentContext } from './extension';
 
 export class FileReader {
-     
+
     public static ReadJSONFromExtensionDir(file: string, encoding?: string):any {
         try {
             let ext = getCurrentContext().extensionPath;
-            
-            let content = fs.readFileSync(path.join(ext, file), encoding);
+            let fileEncoding: string = "";
+
+            if (encoding !== undefined) {
+                fileEncoding = encoding;
+            }
+
+            let content = fs.readFileSync(path.join(ext, file));
             if (content !== null || content !== undefined) {
                 return JSON.parse(content.toString());
             }
