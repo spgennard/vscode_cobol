92c72843de896532fcf1ac54f2304c187931f49d
Stephgen Gennard <stephen@gennard.net>
2020-06-04 13:47:21 +0100

    upd

--- src/test/Program1.cbl
+++
@@ -1,8 +0,0 @@
-       program-id. Program1.
-       working-storage section.
-       01 MyFirstStorageItem       pic x(10).
-       procedure division.
-           
-           goback.
-
-       end program Program1.
