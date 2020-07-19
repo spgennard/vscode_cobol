2981be1a7d28631c3b2296ff7db26d6b4ada0f0e
Stephgen Gennard <stephen@gennard.net>
2020-07-20 00:49:51 +0100

    add auto changelog

--- publishit.sh
+++ publishit.sh
@@ -1,6 +1,43 @@
 # exit on error
 set -e
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+
+npx git-changelog-command-line -std -tec "
+# Changelog
+
+Changelog for {{ownerName}} {{repoName}}.
+
+{{#tags}}
+## {{name}}
+ {{#issues}}
+  {{#hasIssue}}
+   {{#hasLink}}
+### {{name}} [{{issue}}]({{link}}) {{title}} {{#hasIssueType}} *{{issueType}}* {{/hasIssueType}} {{#hasLabels}} {{#labels}} *{{.}}* {{/labels}} {{/hasLabels}}
+   {{/hasLink}}
+   {{^hasLink}}
+### {{name}} {{issue}} {{title}} {{#hasIssueType}} *{{issueType}}* {{/hasIssueType}} {{#hasLabels}} {{#labels}} *{{.}}* {{/labels}} {{/hasLabels}}
+   {{/hasLink}}
+  {{/hasIssue}}
+  {{^hasIssue}}
+### {{name}}
+  {{/hasIssue}}
+
+  {{#commits}}
+**{{{messageTitle}}}**
+
+{{#messageBodyItems}}
+ * {{.}} 
+{{/messageBodyItems}}
+
+[{{hash}}](https://github.com/{{ownerName}}/{{repoName}}/commit/{{hash}}) {{authorName}} *{{commitTime}}*
+
+  {{/commits}}
+
+ {{/issues}}
+{{/tags}}
+" >CHANGELOG.md
+git commit CHANGELOG.md "Update CHANGELOG.md"
+giut push CHNAGELOG.md
 git tag $PACKAGE_VERSION
 git push --tags
 git clean -fdx
