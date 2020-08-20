7c8e6ba24653d7b66416ac2459f62a7792eb9877
Stephgen Gennard <stephen@gennard.net>
2020-08-20 14:31:25 +0100

    start to add some test and hopefully migrate the old ones

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,13 +3,22 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import * as testExtension from '../../extension';
+
+import { FileSourceHandler } from "../../filesourcehandler";
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
+	const baseForSource = __dirname+"/../../../src/test/suite/";
+
+	test('Read file (test.cbl)', () => {
+		console.log("CWD="+__dirname);
+		const f = new FileSourceHandler(baseForSource+"test.cbl",false,false);
+		if (f.lines.length < 10) {
+			assert.fail("test.cbl should have > 10 lines");
+		}
+
 	});
+
 });
---
+++ src/test/suite/test.cbl
@@ -0,0 +1,13 @@
+       program-id. test.
+
+       environment division.
+       configuration section.
+
+       data division.
+       working-storage section.
+
+       procedure division.
+
+           goback.
+
+       end program test.
