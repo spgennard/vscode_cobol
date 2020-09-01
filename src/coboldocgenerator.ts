66a529e9b7d88f43f500782affdd4d086461c988
Stephgen Gennard <stephen@gennard.net>
2020-09-01 18:15:29 +0100

    ASdd "COBOL Source view" that uses html rather than markdown

--- src/coboldocgenerator.ts
+++ src/coboldocgenerator.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import { CobolDocStyle, CobolTagStyle } from './cobolsourcescanner';
+import COBOLSourceScanner, { CobolDocStyle, CobolTagStyle } from './cobolsourcescanner';
 import { logMessage, isDirectory, logChannelSetPreserveFocus } from './extension';
 import VSQuickCOBOLParse from './vscobolscanner';
 import path from 'path';
@@ -11,7 +11,7 @@ import { getWorkspaceFolders } from './cobolfolders';
 
 export class COBOLDocumentationGenerator {
 
-    private static getTempDirectory(settings: ICOBOLSettings) : string {
+    private static getTempDirectory(settings: ICOBOLSettings): string {
         let firstCacheDir = "";
         const ws = getWorkspaceFolders();
         if (ws !== undefined) {
@@ -43,13 +43,20 @@ export class COBOLDocumentationGenerator {
     }
 
 
-    public static showCOBOLDOCDocumentation(activeTextEditor: vscode.TextEditor, settings: ICOBOLSettings):void {
+    public static showCOBOLDOCDocumentation(activeTextEditor: vscode.TextEditor, settings: ICOBOLSettings): void {
 
         const sf = VSQuickCOBOLParse.getCachedObject(activeTextEditor.document);
         if (sf === undefined) {
             return;
         }
 
+        COBOLDocumentationGenerator.generateHTML(settings, sf);
+
+    }
+
+    static cobolDocPanel: vscode.WebviewPanel|undefined = undefined;
+
+    private static generateHTML(settings: ICOBOLSettings, sf: COBOLSourceScanner) {
         const tagStype = sf.commentTagStyle === CobolTagStyle.FREE ? "-s free" : "-s microfocus";
         const tmpArea = COBOLDocumentationGenerator.getTempDirectory(settings);
         if (tmpArea === "") {
@@ -59,10 +66,10 @@ export class COBOLDocumentationGenerator {
             return;
         }
         const textToSend = sf.commentStyle === CobolDocStyle.MSDN ?
-                    `coboldoc -o ${tmpArea} ${tagStype} -a msdn -f md generate ${sf.filename}` :
-                    `coboldoc -o ${tmpArea} ${tagStype} -a tag -f md generate ${sf.filename}`;
+            `coboldoc -o ${tmpArea} ${tagStype} -a msdn -f html generate ${sf.filename}` :
+            `coboldoc -o ${tmpArea} ${tagStype} -a tag -f html generate ${sf.filename}`;
 
-        exec(textToSend,  (error, stdout, stderr) => {
+        exec(textToSend, (error, stdout, stderr) => {
             if (error) {
                 logChannelSetPreserveFocus(false);
                 logMessage(`coboldoc: [${error.message}]`);
@@ -75,11 +82,53 @@ export class COBOLDocumentationGenerator {
             }
 
             logMessage(`coboldoc: ${stdout}`);
+
+            const shortFilename = path.basename(sf.filename);
+            const htmlFilename = `${tmpArea}/${shortFilename}.html`;
+
+            const htmlContents = fs.readFileSync(htmlFilename).toString();
+            if (COBOLDocumentationGenerator.cobolDocPanel === undefined) {
+                COBOLDocumentationGenerator.cobolDocPanel = vscode.window.createWebviewPanel("coboldoc", "COBOL Documentation", vscode.ViewColumn.One);
+            }
+            COBOLDocumentationGenerator.cobolDocPanel.webview.html = htmlContents;
+        });
+    }
+
+    private generateMarkdown(settings: ICOBOLSettings, sf: COBOLSourceScanner) {
+        const tagStype = sf.commentTagStyle === CobolTagStyle.FREE ? "-s free" : "-s microfocus";
+        const tmpArea = COBOLDocumentationGenerator.getTempDirectory(settings);
+        if (tmpArea === "") {
+            logChannelSetPreserveFocus(false);
+            logMessage("Unable to find or create a coboldoc directory");
+
+            return;
+        }
+        const textToSend = sf.commentStyle === CobolDocStyle.MSDN ?
+            `coboldoc -o ${tmpArea} ${tagStype} -a msdn -f md generate ${sf.filename}` :
+            `coboldoc -o ${tmpArea} ${tagStype} -a tag -f md generate ${sf.filename}`;
+
+        exec(textToSend, (error, stdout, stderr) => {
+            if (error) {
+                logChannelSetPreserveFocus(false);
+                logMessage(`coboldoc: [${error.message}]`);
+                return;
+            }
+            if (stderr) {
+                logChannelSetPreserveFocus(false);
+                logMessage(`coboldoc: [${stderr}]`);
+                return;
+            }
+
+            logMessage(`coboldoc: ${stdout}`);
+
             const openPreview = "markdown.showPreview";
             const shortFilename = path.basename(sf.filename);
-            vscode.commands.executeCommand("vscode.open", vscode.Uri.parse(`${tmpArea}/${shortFilename}.md`)).then(() =>
-                vscode.commands.executeCommand(openPreview)
-                );
+            const mdFilename = `${tmpArea}/${shortFilename}.md`;
+
+            vscode.commands.executeCommand("workbench.action.newGroupRight").then(() =>
+                vscode.commands.executeCommand("vscode.open", vscode.Uri.parse(`${mdFilename}`)).then(() =>
+                    vscode.commands.executeCommand(openPreview)
+                ));
         });
     }
 }
