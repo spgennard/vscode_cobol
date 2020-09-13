275fa043497a0393ffefd62fc0783aa4ca21983a
Stephgen Gennard <stephen@gennard.net>
2020-09-13 20:24:43 +0100

    remove typescript version, so I can use the node-html parser (temp measure)  - rewrite style section in the html, so all of the html is displayed

--- src/coboldocgenerator.ts
+++ src/coboldocgenerator.ts
@@ -7,7 +7,7 @@ import fs from 'fs';
 import { exec } from 'child_process';
 import { ICOBOLSettings } from './iconfiguration';
 import { getWorkspaceFolders } from './cobolfolders';
-
+import { parse } from 'node-html-parser';
 
 export class COBOLDocumentationGenerator {
 
@@ -80,7 +80,7 @@ export class COBOLDocumentationGenerator {
             const shortFilename = path.basename(sf.filename);
             const htmlFilename = `${tmpArea}/${shortFilename}.html`;
 
-            const htmlContents = fs.readFileSync(htmlFilename).toString();
+            const htmlContents = COBOLDocumentationGenerator.fixupHTML(fs.readFileSync(htmlFilename).toString());
             if (COBOLDocumentationGenerator.cobolDocPanel === undefined) {
                 COBOLDocumentationGenerator.cobolDocPanel = vscode.window.createWebviewPanel("coboldoc", "COBOL Documentation",
                     vscode.ViewColumn.Beside, { enableScripts: true });
@@ -95,14 +95,37 @@ export class COBOLDocumentationGenerator {
         });
     }
 
+    private static fixupHTML(html: string) : string {
+        const root = parse(html);
+
+        const style = root.querySelector("style");
+        style.set_content('\
+        table {\
+            font-family: "Courier New", Courier, monospace;\
+            border: 2px solid #FFFFFF;\
+            border-collapse: collapse;\
+            width: 90%;\
+          }\
+          table th {\
+            border: 4px solid #0B6FA4;\
+            padding: 3px 2px;\
+            background: #0B6FA4; \
+          }\
+          table td {\
+            border: 4px solid #0B6FA4;\
+            padding: 3px 2px;\
+          }\
+          }');
+
+        return root.toString();
+    }
+
     public static generateCOBOLSourceTag(activeTextEditor: vscode.TextEditor, settings: ICOBOLSettings): void {
 
         const sf: COBOLSourceScanner | undefined = VSQuickCOBOLParse.getCachedObject(activeTextEditor.document);
         if (sf === undefined) {
             return;
         }
-
-        const tagStype = sf.commentTagStyle === CobolTagStyle.FREE ? "-s free" : "-s microfocus";
 
         if (activeTextEditor.selection !== undefined) {
             const currentLineNumber = activeTextEditor.selection.start.line;
@@ -126,7 +149,7 @@ export class COBOLDocumentationGenerator {
                 }
             }
 
-            
+
             logMessage(closestToken.tokenName);
 
             return;
