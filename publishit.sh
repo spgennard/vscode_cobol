c114271d864fe08f9a0dd4a8f9a8a57d818685cc
spgennard <spgennard@users.noreply.github.com>
2021-05-20 11:41:37 +0100

    Update publishit.sh

--- publishit.sh
+++ publishit.sh
@@ -17,18 +17,6 @@ git push
 vsce publish
 #npm run afterpublish
 rm -f *.vsix
-#vsce package
-#COMMIT_LOG=$(git log -1 --format='%ci %H %s')
-#PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
-#github-release upload \
-#  --owner=spgennard \
-#  --repo=vscode_cobol \
-#  --tag="$PACKAGE_VERSION" \
-#  --name=$PACKAGE_VERSION \
-#  --body="${COMMIT_LOG}" \
-#  cobol*.vsix
-#
-#npx ovsx publish cobol*.vsix -p $(cat $HOME/.ovsx.token)
 
 mkdir .vscode-test 2>/dev/null && true
 [ -d "../.vscode_test" ] && cp -r ../.vscode-test .
