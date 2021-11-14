c3b83f422b2ccca4ee3b561fb23bbf5ae9c21db1
Stephgen Gennard <stephen@gennard.net>
2021-11-14 17:30:57 +0000

    tighten code

--- src/vscobolscanner.ts
+++ src/vscobolscanner.ts
@@ -151,6 +151,7 @@ export class VSCOBOLSourceScanner {
                         if (await VSPreProc.registerPreProcessors(config) === false) {
                             return undefined;
                         }
+                        return undefined;
                     };
                 }
 
