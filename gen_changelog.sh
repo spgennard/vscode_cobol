0118ad68e081d2bb04fbb7283fc84dc4820b65e9
Stephgen Gennard <stephen@gennard.net>
2021-12-01 22:37:38 +0000

    move to simplier changelog, as the java version crashes too much

--- gen_changelog.sh
+++ gen_changelog.sh
@@ -1,24 +1,7 @@
 # exit on error
-set -x
-set -e
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
-# --ignore-commits-without-issue
-A=$(npx git-changelog-command-line --to-ref refs/heads/main \
---ignore-pattern "^\[maven-release-plugin\].*|^\[Gradle Release Plugin\].*|^Merge.*|^bump.*" \
---no-issue-name "" -std -tec "# Changelog
 
-Changelog for {{ownerName}}{{repoName}}.
+echo "# CHANGELOG" >CHANGELOG.md
+echo >>CHANGELOG.md
+git log --oneline --decorate $PACKAGE_VERSION | xargs -d\\n -n 1 echo '*' >>CHANGELOG.md
 
-{{#tags}}
-## {{name}} {{#issues}} {{#hasIssue}} {{#hasLink}}
-### {{name}} [{{issue}}]({{link}}) {{title}} {{#hasIssueType}} *{{issueType}}* {{/hasIssueType}} {{#hasLabels}} {{#labels}} *{{.}}* {{/labels}} {{/hasLabels}} {{/hasLink}} {{^hasLink}}
-### {{name}} {{issue}} {{title}} {{#hasIssueType}} *{{issueType}}* {{/hasIssueType}} {{#hasLabels}} {{#labels}} *{{.}}* {{/labels}} {{/hasLabels}} {{/hasLink}} {{/hasIssue}}
-  {{#commits}}
-**{{{messageTitle}}}**
-* [{{hash}}](https://github.com/{{ownerName}}/{{repoName}}/commit/{{hash}}) *{{commitTime}}*
-  {{/commits}}
- {{/issues}}
-{{/tags}}
-")
-
-echo -e $A | awk '!NF {if (++n <= 2) print; next}; {n=0;print}' >CHANGELOG.md
