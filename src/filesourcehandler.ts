87958cc712f2c1e045ef27055c81289c801a4956
Stephgen Gennard <stephen@gennard.net>
2022-01-14 21:22:32 +0000

    flip substr to substring

--- src/filesourcehandler.ts
+++ src/filesourcehandler.ts
@@ -129,19 +129,19 @@ export class FileSourceHandler implements ISourceHandler {
             // todo - this is a bit messy and should be revised
             if (this.dumpNumbersInAreaA) {
                 if (line.match(FileSourceHandler.paraPrefixRegex1)) {
-                    line = "      " + line.substr(6);
+                    line = "      " + line.substring(6);
                 } else {
                     if (line.length > 7 && line[6] === " ") {
-                        const possibleKeyword = line.substr(0, 6).trim();
+                        const possibleKeyword = line.substring(0, 6).trim();
                         if (this.isValidKeyword(possibleKeyword) === false) {
-                            line = "       " + line.substr(6);
+                            line = "       " + line.substring(6);
                         }
                     }
                 }
             }
             
             if (this.dumpAreaBOnwards && line.length >= 73) {
-                line = line.substr(0, 72);
+                line = line.substring(0, 72);
             }
         }
         catch {
