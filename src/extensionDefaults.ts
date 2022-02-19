6bab561692bb463d03eadb7636e2ccbc8fa02164
Stephgen Gennard <stephen@gennard.net>
2022-02-19 20:59:34 +0000

    add more help with conflicts

--- src/extensionDefaults.ts
+++ src/extensionDefaults.ts
@@ -2,4 +2,7 @@ export class ExtensionDefaults {
     public static readonly defaultCOBOLLanguage = "COBOL";
     public static readonly defaultEditorConfig = "coboleditor";
     public static readonly thisExtensionName = "bitlang.cobol";
+
+    public static readonly microFocusCOBOLExtension = "micro-focus-amc.mfcobol";
+    public static readonly microFocusEnterpriseExtenstion = "micro-focus-amc.mfenterprise";
 }
