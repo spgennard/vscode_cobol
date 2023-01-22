95c9152781e4c3a5a6647ff24a54cb08f5368d5c
Stephen Gennard <stephen@gennard.net>
2023-01-22 20:29:53 +0000

    add some diag routines (temp)

---
+++ src/vsdiagcommands.ts
@@ -0,0 +1,30 @@
+import * as vscode from "vscode";
+import { VSLogger } from "./vslogger";
+
+export class VSDiagCommands {
+
+    private static dumpSymbol(depth: string, symbol: vscode.DocumentSymbol) {
+        const symbolKind = vscode.SymbolKind[symbol.kind];
+
+        VSLogger.logMessage(`${depth} Symbol : "${symbol.name}" of ${symbolKind} "${symbol.detail}" (${symbol.tags})`);
+        for (const childSymbol of symbol.children) {
+            VSDiagCommands.dumpSymbol(depth + " ", childSymbol);
+        }
+    }
+    public static async DumpAllSymbols() {
+        if (vscode.window.activeTextEditor) {
+            const activeUrl = vscode.window.activeTextEditor.document.uri;
+            const symbolsArray = await vscode.commands.executeCommand<[]>("vscode.executeDocumentSymbolProvider", activeUrl, "*");
+
+            VSLogger.logMessage("Document Symbols found:")
+            const symbols = symbolsArray as vscode.DocumentSymbol[];
+            for (const symbol of symbols) {
+                try {
+                    VSDiagCommands.dumpSymbol("", symbol);
+                } catch (e) {
+                    VSLogger.logException(`Symbol: ${symbol.name}`, e as Error);
+                }
+            }
+        }
+    }
+}
