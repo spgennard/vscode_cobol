52fe2b1e1f03f92cc1962b75b0d0680ce28f9bfb
Stephen Gennard <stephen@gennard.net>
2023-12-22 22:37:54 +0000

    add better copybook defaults

--- src/vsdirectivesconv.ts
+++ src/vsdirectivesconv.ts
@@ -1,14 +1,24 @@
- 
- const _gridOptions:Map<string, string> = 
-    new Map([
-        [">>CALL-CONVENTION COBOL", ""],
-        [">>CALL-CONVENTION EXTERN", ""],
-        [">>CALL-CONVENTION STDCALL", ""],
-        [">>CALL-CONVENTION STATIC", ""],
-        
-    ]);
 
-export function getGrip()
-{
-    return _gridOptions;
+type portSwap = [RegExp, string];
+export type portResult = [boolean, string];
+
+const _gnuDirectives : portSwap[] = 
+    [
+        [/>>CALL-CONVENTION COBOL/, ""],
+        [/>>CALL-CONVENTION EXTERN/, ""],
+        [/>>CALL-CONVENTION STDCALL/, ""],
+        [/>>CALL-CONVENTION STATIC/, ""]
+    ];
+
+export function gnuPortActionRequired(line: string) : portResult {
+    for(const gnuDirctive of _gnuDirectives)
+    {
+        if (gnuDirctive[0].test(line))
+        {
+            const repResult = line.replace(gnuDirctive[0],gnuDirctive[1]);
+            return [ true, repResult];
+        }
+    }
+
+    return [false, ""];
 }
