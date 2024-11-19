0f4bffd2852554172e29997914148d1a0eb53749
Stephen Gennard <stephen@gennard.net>
2024-11-19 18:11:08 +0000

    tweak comment to work better, re-structure to make it cleaner

--- src/commenter.ts
+++ src/commenter.ts
@@ -25,103 +25,112 @@ export class commentUtils {
         const line = d.lineAt(l);
         const lineContents = line.text;
 
-        if (format === ESourceFormat.terminal) {
-            const firstInlineIndex = lineContents.indexOf("|");
-            if (firstInlineIndex !== -1) {
-                const r = new Range(new Position(l, firstInlineIndex), new Position(l, 2 + firstInlineIndex));
-                editor.delete(r);
-                return;
-            }
-            let defpos = -1;
-            for (let w = 0; w < lineContents.length; w++) {
-                if (lineContents[w] === " ") {
-                    defpos++;
-                }
-                else {
-                    break;
-                }
-            }
-
-            if (defpos !== -1) {
-                editor.insert(new Position(l, 1 + defpos), "| ");
-                return;
-            }
-
-            return;
-        }
-
-        if (format !== ESourceFormat.fixed) {
-            let firstInlineIndex = lineContents.indexOf("*> ");
-            if (firstInlineIndex !== -1 && firstInlineIndex !== 6) {
-                const r = new Range(new Position(l, firstInlineIndex), new Position(l, 3 + firstInlineIndex));
-                editor.delete(r);
-                return;
-            }
-
-            firstInlineIndex = lineContents.indexOf("*>");
-            if (firstInlineIndex !== -1) {
-                const r = new Range(new Position(l, firstInlineIndex), new Position(l, 2 + firstInlineIndex));
-                editor.delete(r);
-                return;
-            }
-
-            let defpos = -1;
-            for (let w = 0; w < lineContents.length; w++) {
-                if (lineContents[w] === " ") {
-                    defpos++;
-                }
-                else {
-                    break;
-                }
-            }
-
-            if (defpos !== -1) {
-                // if we have a fixed column comment, convert it..
-                if (defpos === 5 && lineContents[6] === "*") {
-                    editor.insert(new Position(l, 7), "> ");
-                    return;
-                }
-
-                // we can use the comment column
-                if (defpos === 6) {   // defpos started at -1....
-                    editor.insert(new Position(l, 6), "*>");
-                    return;
-                }
-
-                if (commentUtils.var_free_insert_at_comment_column && defpos > 6) {
-                    if (lineContents[6] === " " && lineContents[7] === " " && lineContents[8] === " ") {
-                        editor.insert(new Position(l, 6), "*>");
+        switch (format) {
+            case ESourceFormat.terminal:
+                {
+                    const firstInlineIndex = lineContents.indexOf("|");
+                    if (firstInlineIndex !== -1) {
+                        const r = new Range(new Position(l, firstInlineIndex), new Position(l, 2 + firstInlineIndex));
+                        editor.delete(r);
                         return;
                     }
+                    let defpos = -1;
+                    for (let w = 0; w < lineContents.length; w++) {
+                        if (lineContents[w] === " ") {
+                            defpos++;
+                        }
+                        else {
+                            break;
+                        }
+                    }
+
+                    if (defpos !== -1) {
+                        editor.insert(new Position(l, 1 + defpos), "| ");
+                        return;
+                    }
+
+                    editor.insert(new Position(l, 0), "| ");
+                    return;
                 }
 
-                editor.insert(new Position(l, 1 + defpos), "*> ");
-                return;
-            }
+            case ESourceFormat.fixed:
+                {
+                    /* remove * from column 6 */
+                    if (lineContents.length > 6 &&
+                        (lineContents[6] === "*" || lineContents[6] === "/")) {
+                        const r = new Range(new Position(l, 6), new Position(l, 7));
+                        editor.replace(r, " ");
+                        return;
+                    }
 
-            if (lineContents.length > 6 && lineContents[6] === " " && lineContents[7] === " ") {
-                const r = new Range(new Position(l, 6), new Position(l, 8));
-                editor.replace(r, "*>");
-                return;
-            }
+                    if (lineContents.length > 6 && lineContents[6] === " ") {
+                        const r = new Range(new Position(l, 6), new Position(l, 7));
+                        editor.replace(r, "*");
+                        return;
+                    }
+                    return;
+                }
+            case ESourceFormat.free:
+            case ESourceFormat.variable:
+                {
+                    let firstInlineIndex = lineContents.indexOf("*> ");
+                    if (firstInlineIndex !== -1 && firstInlineIndex !== 6) {
+                        const r = new Range(new Position(l, firstInlineIndex), new Position(l, 3 + firstInlineIndex));
+                        editor.delete(r);
+                        return;
+                    }
 
-            return;
+                    firstInlineIndex = lineContents.indexOf("*>");
+                    if (firstInlineIndex !== -1) {
+                        const r = new Range(new Position(l, firstInlineIndex), new Position(l, 2 + firstInlineIndex));
+                        editor.delete(r);
+                        return;
+                    }
+
+                    let defpos = -1;
+                    for (let w = 0; w < lineContents.length; w++) {
+                        if (lineContents[w] === " ") {
+                            defpos++;
+                        }
+                        else {
+                            break;
+                        }
+                    }
+
+                    if (defpos !== -1) {
+                        // if we have a fixed column comment, convert it..
+                        if (defpos === 5 && lineContents[6] === "*") {
+                            editor.insert(new Position(l, 7), "> ");
+                            return;
+                        }
+
+                        // we can use the comment column
+                        if (defpos === 6) {   // defpos started at -1....
+                            editor.insert(new Position(l, 6), "*>");
+                            return;
+                        }
+
+                        if (commentUtils.var_free_insert_at_comment_column && defpos > 6) {
+                            if (lineContents[6] === " " && lineContents[7] === " " && lineContents[8] === " ") {
+                                editor.insert(new Position(l, 6), "*>");
+                                return;
+                            }
+                        }
+
+                        editor.insert(new Position(l, 1 + defpos), "*> ");
+                        return;
+                    }
+
+                    if (lineContents.length > 6 && lineContents[6] === " " && lineContents[7] === " ") {
+                        const r = new Range(new Position(l, 6), new Position(l, 8));
+                        editor.replace(r, "*>");
+                        return;
+                    }
+
+                    editor.insert(new Position(l, 0), "*> ");
+                    return;
+                }
         }
-
-        /* remove * from column 6 */
-        if (lineContents.length > 6 &&
-            (lineContents[6] === "*" || lineContents[6] === "/")) {
-            const r = new Range(new Position(l, 6), new Position(l, 7));
-            editor.replace(r, " ");
-            return;
-        }
-
-        if (lineContents.length > 6 && lineContents[6] === " ") {
-            const r = new Range(new Position(l, 6), new Position(l, 7));
-            editor.replace(r, "*");
-            return;
-        }
-
     }
 
     private static multipleToggleLine(edit: TextEditorEdit, d: TextDocument, sel: Selection, format: ESourceFormat) {
@@ -137,7 +146,7 @@ export class commentUtils {
             const sels = editor.selections;
             let sourceformatStyle: ESourceFormat = ESourceFormat.variable;
 
-            const gcp = VSCOBOLSourceScanner.getCachedObject(doc,settings);
+            const gcp = VSCOBOLSourceScanner.getCachedObject(doc, settings);
             if (gcp !== undefined) {
                 sourceformatStyle = gcp.sourceFormat;
             }
