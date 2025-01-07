280ab58f23248778496e50ac7d0f66ea19c5f2fa
Stephen Gennard <stephen@gennard.net>
2025-01-07 23:30:03 +0000

    tweak

--- src/vsdotmarkdown.ts
+++ src/vsdotmarkdown.ts
@@ -74,7 +74,7 @@ export class DotGraphPanelView {
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <meta http-equiv="Content-Security-Policy" 
+    <meta http-equiv="Content-Security-Policy" 
         content="default-src 'none'; 
         style-src cspSource 'unsafe-inline';
         script-src 'nonce-nonce' 'unsafe-inline';
